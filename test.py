b052f80ea8a8349773e852e41e3003d7e0a6c59e
Max Levin <110128097+malevi413@users.noreply.github.com>
2022-10-02 22:02:51 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 import pytest
-import time
-from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
